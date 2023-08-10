3aa6644c9c2b79f37007be92592ecc851eb7209e
VELAHKING <dankim2019@gmail.com>
2023-08-10 05:59:41 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a c program
+ * Return: 1 (successful)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+}

3bdeda762254b12ae9ac5d1a4dfa9201cd19ca63
VELAHKING <dankim2019@gmail.com>
2023-08-10 03:22:21 -0700

    first program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - my first c program
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

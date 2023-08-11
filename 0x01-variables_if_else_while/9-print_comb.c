7709496c6a7efe556afcf8a0ac0e94f44d65a22e
VELAHKING <dankim2019@gmail.com>
2023-08-11 01:58:38 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.jh>
+/**
+ * main - main function
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		putchar(i + '0');
+		puchar(',');
+		putchar(' ');
+	}
+	return (0);
+}

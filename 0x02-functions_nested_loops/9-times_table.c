964f1c0a845056cf72d42d8150af307d21c9291a
VELAHKING <dankim2019@gmail.com>
2023-08-15 05:11:26 -0700

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints multiplication table
+ * Return: empty
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+	int a;
+	int b;
+
+	for (x = 0; x <= 9; x++)
+	{
+	for (y = 0; y <= 9; y++)
+	{
+	z = x * y;
+	if (z > 9)
+	{
+	a = z % 10;
+	b = (z - a) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(b + '0');
+	_putchar(a + '0');
+	}
+	else
+	{
+	if (y != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(z + '0');
+	}
+	}
+	_putchar('\n');
+	}
+}

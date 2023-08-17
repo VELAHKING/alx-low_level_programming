cfae77b09709968fdf57a2cf0798544fcfd5719e
VELAHKING <dankim2019@gmail.com>
2023-08-17 10:22:27 -0700

    ./101-print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,26 @@
+#include "main.h"
 
+/**
+ * print_number - print a number
+ * @n: number to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int a1;
+
+	if (n < 0)
+	{
+		a1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		a1 = n;
+	}
+	if (a1 / 10)
+	{
+		print_number(a1 / 10);
+	}
+	_putchar((a1 % 10) + '0');
+}
